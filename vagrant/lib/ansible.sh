45909e055d70bb84fa32063e31cdf8e4f2d6be17
Dethroner <dethronerby@gmail.com>
2018-01-29 12:49:15 +0300

    Testing VM+

---
+++ vagrant/lib/ansible.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+apt-get update -y
+apt-get install -y build-essential \
+	mc \
+	htop \
+  libxml2-dev \
+  libxslt-dev \
+  libffi-dev \
+  libssl-dev \
+  python2.7 \
+  python2.7-dev \
+  python-lxml \
+  software-properties-common \
+  git
+
+# Install ansible
+wget -c https://bootstrap.pypa.io/get-pip.py
+python get-pip.py
+pip install ansible
+
+cat /vagrant/ssh/ansible.cfg > /home/dethroner/.ansible.cfg
+chmod 640 /home/dethroner/.ansible.cfg
+chown dethroner:dethroner /home/dethroner/.ansible.cfg
+
+# Auto cleanup
+apt-get autoremove -y
