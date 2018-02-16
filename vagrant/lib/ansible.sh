d94abf429372dde585d705c805d55dd8908da926
Dethroner <dethronerby@gmail.com>
2018-02-16 12:09:12 +0300

    Ansible vs Docker role

--- vagrant/lib/ansible.sh
+++ vagrant/lib/ansible.sh
@@ -2,8 +2,8 @@
 
 apt-get update -y
 apt-get install -y build-essential \
-	mc \
-	htop \
+  mc \
+  htop \
   libxml2-dev \
   libxslt-dev \
   libffi-dev \
@@ -12,16 +12,20 @@ apt-get install -y build-essential \
   python2.7-dev \
   python-lxml \
   software-properties-common \
-  git
+  git \
+  sshpass
 
 # Install ansible
 wget -c https://bootstrap.pypa.io/get-pip.py
 python get-pip.py
 pip install ansible
 
-cat /vagrant/ssh/ansible.cfg > /home/dethroner/.ansible.cfg
+cat /vagrant/ansible/ansible.cfg > /home/dethroner/.ansible.cfg
 chmod 640 /home/dethroner/.ansible.cfg
 chown dethroner:dethroner /home/dethroner/.ansible.cfg
+#mkdir -p /etc/ansible/
+#touch /etc/ansible/hosts
+#cat /vagrant/ansible/nodes >> /etc/ansible/hosts
 
 # Auto cleanup
 apt-get autoremove -y
